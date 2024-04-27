93ef99971a8618e6a87b0f2424917bda215d9598
Bruno Kambere <brunokambere@gmail.com>
2024-04-27 19:19:22 +0200

    Setting up the landing page

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/jobs', function () {
+    // Redirect to the specified view
+    return view('jobs.job-listing');
+});
+
+Route::get('/job', function () {
+    // Redirect to the specified view
+    return view('jobs.job-single');
+});
