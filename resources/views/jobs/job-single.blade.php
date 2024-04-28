df8597bc5acf0a0efd8a71e1f842808314e3cdd7
Bruno Kambere <brunokambere@gmail.com>
2024-04-28 13:48:30 +0200

    Enhance job section

--- resources/views/jobs/job-single.blade.php
+++ resources/views/jobs/job-single.blade.php
@@ -87,7 +87,7 @@
               </div>
 
               <h2 class="entry-title">
-                <a href="blog-single.html">Dolorum optio tempore voluptas dignissimos cumque fuga qui quibusdam quia</a>
+                <a href="blog-single.html">Blockchain Developer.</a>
               </h2>
 
               <div class="entry-meta">
@@ -102,42 +102,51 @@
 
               <div class="entry-content">
                 <p>
-                  Similique neque nam consequuntur ad non maxime aliquam quas. Quibusdam animi praesentium. Aliquam et laboriosam eius aut nostrum quidem aliquid dicta.
-                  Et eveniet enim. Qui velit est ea dolorem doloremque deleniti aperiam unde soluta. Est cum et quod quos aut ut et sit sunt. Voluptate porro consequatur assumenda perferendis dolore.
+                A Blockchain Developer specializes in building decentralized applications (DApps) 
+                and smart contracts using blockchain technology. They possess expertise in blockchain platforms like Ethereum, 
                 </p>
 
                 <p>
-                  Sit repellat hic cupiditate hic ut nemo. Quis nihil sunt non reiciendis. Sequi in accusamus harum vel aspernatur. Excepturi numquam nihil cumque odio. Et voluptate cupiditate.
+                Hyperledger, or EOS, and are proficient in programming languages such as Solidity, JavaScript, or Go.
                 </p>
 
-                <blockquote>
-                  <p>
-                    Et vero doloremque tempore voluptatem ratione vel aut. Deleniti sunt animi aut. Aut eos aliquam doloribus minus autem quos.
-                  </p>
-                </blockquote>
-
                 <p>
-                  Sed quo laboriosam qui architecto. Occaecati repellendus omnis dicta inventore tempore provident voluptas mollitia aliquid. Id repellendus quia. Asperiores nihil magni dicta est suscipit perspiciatis. Voluptate ex rerum assumenda dolores nihil quaerat.
-                  Dolor porro tempora et quibusdam voluptas. Beatae aut at ad qui tempore corrupti velit quisquam rerum. Omnis dolorum exercitationem harum qui qui blanditiis neque.
-                  Iusto autem itaque. Repudiandae hic quae aspernatur ea neque qui. Architecto voluptatem magni. Vel magnam quod et tempora deleniti error rerum nihil tempora.
+                Blockchain Developers 
+                design and implement secure and transparent solutions for various use cases, including cryptocurrency transactions, supply chain management, and identity verification. They work on the development, testing, and deployment of blockchain-based 
+                solutions while ensuring scalability, reliability, and data integrity.
                 </p>
 
-                <h3>Et quae iure vel ut odit alias.</h3>
+                <h3>Requirements:</h3>
                 <p>
-                  Officiis animi maxime nulla quo et harum eum quis a. Sit hic in qui quos fugit ut rerum atque. Optio provident dolores atque voluptatem rem excepturi molestiae qui. Voluptatem laborum omnis ullam quibusdam perspiciatis nulla nostrum. Voluptatum est libero eum nesciunt aliquid qui.
-                  Quia et suscipit non sequi. Maxime sed odit. Beatae nesciunt nesciunt accusamus quia aut ratione aspernatur dolor. Sint harum eveniet dicta exercitationem minima. Exercitationem omnis asperiores natus aperiam dolor consequatur id ex sed. Quibusdam rerum dolores sint consequatur quidem ea.
-                  Beatae minima sunt libero soluta sapiente in rem assumenda. Et qui odit voluptatem. Cum quibusdam voluptatem voluptatem accusamus mollitia aut atque aut.
+                - Bachelor's degree in Computer Science, Information Technology, or related field (or equivalent experience).
+                - Proven experience in software development, preferably in blockchain-related projects.
+                - Understanding of blockchain technology fundamentals, including decentralized systems, distributed ledger technology, and consensus algorithms.
+                - Familiarity with blockchain platforms such as Ethereum, Hyperledger, or EOS.
+                - Proficiency in programming languages commonly used in blockchain development, such as Solidity, JavaScript, or Go.
+                - Strong understanding of cryptographic principles and protocols.
+                - Experience with smart contract development and deployment.
+                - Knowledge of blockchain development tools and frameworks (e.g., Truffle, Remix, Web3.js).
+                - Ability to work with relational and non-relational databases.
+                - Excellent problem-solving and analytical skills.
+                - Strong communication and collaboration abilities.
                 </p>
                 <img src="assets/img/blog/blog-inside-post.jpg" class="img-fluid" alt="">
 
-                <h3>Ut repellat blanditiis est dolore sunt dolorum quae.</h3>
+                <h3>Skills:</h3>
                 <p>
-                  Rerum ea est assumenda pariatur quasi et quam. Facilis nam porro amet nostrum. In assumenda quia quae a id praesentium. Quos deleniti libero sed occaecati aut porro autem. Consectetur sed excepturi sint non placeat quia repellat incidunt labore. Autem facilis hic dolorum dolores vel.
-                  Consectetur quasi id et optio praesentium aut asperiores eaque aut. Explicabo omnis quibusdam esse. Ex libero illum iusto totam et ut aut blanditiis. Veritatis numquam ut illum ut a quam vitae.
+                - Solid understanding of blockchain concepts and architecture.
+                - Proficiency in programming languages like Solidity, JavaScript, or Go.
+                - Experience with smart contract development and deployment.
+                - Familiarity with blockchain platforms such as Ethereum, Hyperledger, or EOS.
+                - Knowledge of cryptographic principles and protocols.
+                - Ability to work with blockchain development tools and frameworks.
+                - Strong understanding of decentralized systems and consensus algorithms.
+                - Experience with version control systems (e.g., Git).
+                - Familiarity with DevOps practices for blockchain development.
+                - Excellent problem-solving and debugging skills.
+                - Strong communication and teamwork abilities.
                 </p>
-                <p>
-                  Alias quia non aliquid. Eos et ea velit. Voluptatem maxime enim omnis ipsa voluptas incidunt. Nulla sit eaque mollitia nisi asperiores est veniam.
-                </p>
+                
 
               </div>
 
@@ -149,15 +158,15 @@
 
                 <i class="bi bi-tags"></i>
                 <ul class="tags">
-                  <li><a href="#">Creative</a></li>
-                  <li><a href="#">Tips</a></li>
+                  <li><a href="#">DevOps</a></li>
+                  <li><a href="#">Web3</a></li>
                   <li><a href="#">Marketing</a></li>
                 </ul>
               </div>
 
             </article><!-- End blog entry -->
 
-            <div class="blog-author d-flex align-items-center">
+            <!-- <div class="blog-author d-flex align-items-center">
               <img src="assets/img/blog/blog-author.jpg" class="rounded-circle float-left" alt="">
               <div>
                 <h4>Jane Smith</h4>
@@ -170,7 +179,7 @@
                   Itaque quidem optio quia voluptatibus dolorem dolor. Modi eum sed possimus accusantium. Quas repellat voluptatem officia numquam sint aspernatur voluptas. Esse et accusantium ut unde voluptas.
                 </p>
               </div>
-            </div><!-- End blog author bio -->
+            </div>End blog author bio -->
 
           </div><!-- End blog entries list -->
 
@@ -202,31 +211,19 @@
               <div class="sidebar-item recent-posts">
                 <div class="post-item clearfix">
                   <img src="assets/img/blog/blog-recent-1.jpg" alt="">
-                  <h4><a href="blog-single.html">Nihil blanditiis at in nihil autem</a></h4>
+                  <h4><a href="blog-single.html">Data Scientist:</a></h4>
                   <time datetime="2020-01-01">Jan 1, 2020</time>
                 </div>
 
                 <div class="post-item clearfix">
                   <img src="assets/img/blog/blog-recent-2.jpg" alt="">
-                  <h4><a href="blog-single.html">Quidem autem et impedit</a></h4>
+                  <h4><a href="blog-single.html">UX/UI Designer:</a></h4>
                   <time datetime="2020-01-01">Jan 1, 2020</time>
                 </div>
 
                 <div class="post-item clearfix">
                   <img src="assets/img/blog/blog-recent-3.jpg" alt="">
-                  <h4><a href="blog-single.html">Id quia et et ut maxime similique occaecati ut</a></h4>
-                  <time datetime="2020-01-01">Jan 1, 2020</time>
-                </div>
-
-                <div class="post-item clearfix">
-                  <img src="assets/img/blog/blog-recent-4.jpg" alt="">
-                  <h4><a href="blog-single.html">Laborum corporis quo dara net para</a></h4>
-                  <time datetime="2020-01-01">Jan 1, 2020</time>
-                </div>
-
-                <div class="post-item clearfix">
-                  <img src="assets/img/blog/blog-recent-5.jpg" alt="">
-                  <h4><a href="blog-single.html">Et dolores corrupti quae illo quod dolor</a></h4>
+                  <h4><a href="blog-single.html">Blockchain Developer:</a></h4>
                   <time datetime="2020-01-01">Jan 1, 2020</time>
                 </div>
 
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -576,7 +576,7 @@
             <div class="post-box">
               <div class="post-img"><img src="assets/img/blog/blog-1.jpg" class="img-fluid" alt=""></div>
               <span class="post-date">Tue, September 15</span>
-              <h3 class="post-title">Eum ad dolor et. Autem aut fugiat debitis voluptatem consequuntur sit</h3>
+              <h3 class="post-title">Data Scientis</h3>
               <a href="{{ url('/job') }}" class="readmore stretched-link mt-auto"><span>Read More</span><i class="bi bi-arrow-right"></i></a>
             </div>
           </div>
@@ -585,7 +585,7 @@
             <div class="post-box">
               <div class="post-img"><img src="assets/img/blog/blog-2.jpg" class="img-fluid" alt=""></div>
               <span class="post-date">Fri, August 28</span>
-              <h3 class="post-title">Et repellendus molestiae qui est sed omnis voluptates magnam</h3>
+              <h3 class="post-title">UX/UI Designer</h3>
               <a href="{{ url('/job') }}" class="readmore stretched-link mt-auto"><span>Read More</span><i class="bi bi-arrow-right"></i></a>
             </div>
           </div>
@@ -594,7 +594,7 @@
             <div class="post-box">
               <div class="post-img"><img src="assets/img/blog/blog-3.jpg" class="img-fluid" alt=""></div>
               <span class="post-date">Mon, July 11</span>
-              <h3 class="post-title">Quia assumenda est et veritatis aut quae</h3>
+              <h3 class="post-title">Blockchain Developer</h3>
               <a href="{{ url('/job') }}" class="readmore stretched-link mt-auto"><span>Read More</span><i class="bi bi-arrow-right"></i></a>
             </div>
           </div>
